4d958495b9b9ac4602a31ce5895c55de93cc42bd
Oscar Eduardo Perez Valenzuela <oscarp819@gmail.com>
2025-09-27 14:53:52 -0500

    Push conexión api con DB Posgre

---
+++ user-api/schema.sql
@@ -0,0 +1,11 @@
+-- 1) Crear un esquema específico para Users
+    CREATE SCHEMA IF NOT EXISTS users_schema AUTHORIZATION master;
+    -- 2) Crear la tabla dentro del esquema
+    CREATE TABLE IF NOT EXISTS users_schema.users (
+    id SERIAL PRIMARY KEY,
+    name  TEXT NOT NULL,
+    email TEXT UNIQUE NOT NULL
+    );
+    -- 3) (Opcional) Dar privilegios al usuario administrador
+    GRANT ALL PRIVILEGES ON SCHEMA users_schema TO master;
+    GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA users_schema TO master;
